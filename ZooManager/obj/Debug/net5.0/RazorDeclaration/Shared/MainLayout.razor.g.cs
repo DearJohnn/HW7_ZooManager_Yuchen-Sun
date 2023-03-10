ee42a243e3da31f79066e04d3ace9a455c5d5c6d
DearJohnn <sun.yuch@northeastern.edu>
2023-03-10 18:06:18 -0500

    1

--- ZooManager/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ ZooManager/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace ZooManager.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\6308\C#\Week7\W7_ZooManager_HW\ZooManager\_Imports.razor"
+#line 1 "D:\6308\C#\Week7\HW7_Zoo Manager_Yuchen Sun\ZooManager\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\6308\C#\Week7\W7_ZooManager_HW\ZooManager\_Imports.razor"
+#line 2 "D:\6308\C#\Week7\HW7_Zoo Manager_Yuchen Sun\ZooManager\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\6308\C#\Week7\W7_ZooManager_HW\ZooManager\_Imports.razor"
+#line 3 "D:\6308\C#\Week7\HW7_Zoo Manager_Yuchen Sun\ZooManager\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\6308\C#\Week7\W7_ZooManager_HW\ZooManager\_Imports.razor"
+#line 4 "D:\6308\C#\Week7\HW7_Zoo Manager_Yuchen Sun\ZooManager\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\6308\C#\Week7\W7_ZooManager_HW\ZooManager\_Imports.razor"
+#line 5 "D:\6308\C#\Week7\HW7_Zoo Manager_Yuchen Sun\ZooManager\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\6308\C#\Week7\W7_ZooManager_HW\ZooManager\_Imports.razor"
+#line 6 "D:\6308\C#\Week7\HW7_Zoo Manager_Yuchen Sun\ZooManager\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\6308\C#\Week7\W7_ZooManager_HW\ZooManager\_Imports.razor"
+#line 7 "D:\6308\C#\Week7\HW7_Zoo Manager_Yuchen Sun\ZooManager\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\6308\C#\Week7\W7_ZooManager_HW\ZooManager\_Imports.razor"
+#line 8 "D:\6308\C#\Week7\HW7_Zoo Manager_Yuchen Sun\ZooManager\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\6308\C#\Week7\W7_ZooManager_HW\ZooManager\_Imports.razor"
+#line 9 "D:\6308\C#\Week7\HW7_Zoo Manager_Yuchen Sun\ZooManager\_Imports.razor"
 using ZooManager;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\6308\C#\Week7\W7_ZooManager_HW\ZooManager\_Imports.razor"
+#line 10 "D:\6308\C#\Week7\HW7_Zoo Manager_Yuchen Sun\ZooManager\_Imports.razor"
 using ZooManager.Shared;
 
 #line default
